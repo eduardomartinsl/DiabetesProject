79739078eb3895b35138305732a3161520f458d8
Eduardo M <emartinslemos@gmail.com>
2017-08-17 11:11:29 -0300

    Inserção do timer no estágio estomago

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour {
+
+    public float tempoRestante = 40f;
+
+    private Text TextoRestante;
+    //public GameObject PanelTimer;
+
+	// Use this for initialization
+	void Start () {
+        //PanelTimer = GameObject.Find("PanelContador");
+        TextoRestante = gameObject.GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempoRestante -= Time.deltaTime;
+        TextoRestante.text = tempoRestante.ToString("n2");
+        if (tempoRestante < 0)
+        {
+            Time.timeScale = 0;
+        }
+	}
+}
